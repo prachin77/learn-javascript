2f47da5d9f04946838fe1457e7ea57bc23cfc7a4
prachin007 <prachinnayak07@gmail.com>
2023-11-24 18:37:02 +0530

    react added

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
-function myfunc() {    
-    let name = "hi";
-    console.log("hello");
-    return "hello";
-}
+// function name(...names) {
+//     return names;
+// }
+// // ... returns array
+// console.log(name("a","b","c"));
+ 
+// // passing objs in func 
+// ob ={
+//     id:2,
+//     name:"kohli"
+// }
+// function object(obj) {
+//     console.log("hello "+obj.name+" your id is "+obj.id);
+// }
+// object(ob)
+
+
+// function chai() {
+//     console.log(this);
+// }
+
+// chai()
+
+
+// let user={
+//     id:1,
+//     name:function(){
+//         console.log(`${this.id}`);
+//     }
+// }
+
+// user.name()
+// user.id=2 
+// user.name()
+
+
+// function chai(){
+//     let id=1;
+//     console.log(this.id);
+// }
+// chai()
+
+// let chai = function (){
+//     let id=2
+//     console.log(this.id);
+// }
+// chai()
+
+// let chai = () => {
+//     let id=2
+//     console.log(this.id);
+// }
+// chai()
+
+function Video({ video }) {
+    return (
+      <div>
+        <Thumbnail video={video} />
+        <a href={video.url}>
+          <h3>{video.title}</h3>
+          <p>{video.description}</p>
+        </a>
+        <LikeButton video={video} />
+      </div>
+    );
+  }
