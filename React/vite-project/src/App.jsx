aef83f042cf6c8ba573bdc63b577fc7f06eecc04
prachin007 <prachinnayak07@gmail.com>
2023-11-29 15:56:02 +0530

    bg color changed by butts in project 1

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 
-function MyButton(){
-  function Handle(){
-    alert("button clicked");
-  }
-  return(
-    <>
-     <button onClick={Handle}>click here</button>
-    </>
-  );
-}
 
-export default function myApp(){
+export default function App(){
+  // let back="https://cdn-icons-png.flaticon.com/128/3916/3916837.png"
+  let [color,setColor] = useState('bisque')
+  let changeColor=(value)=>{
+    setColor(value)
+  }
+
   return(
-    <div>
-      <MyButton/>
+    <div className="screen" style={{backgroundColor:color}}>
+      <div className='bstrip'>
+        <button className='butts'  
+          style={{backgroundColor:"bisque"}} 
+          onClick={()=>changeColor("bisque")}>
+          default
+          <img src="https://cdn-icons-png.flaticon.com/128/3916/3916837.png"
+            style={{height:"20px"}}
+          ></img>
+        </button>
+        
+        <button className='butts'  style={{backgroundColor:"white"}} onClick={()=>changeColor("white")}>white</button>
+        <button className='butts'  style={{backgroundColor:"black",color:"white"}} onClick={()=>changeColor("black")}>black</button>
+        <button className='butts'  style={{backgroundColor:"green"}} onClick={()=>changeColor("green")}>green</button>
+      </div>
     </div>
   );
 }
