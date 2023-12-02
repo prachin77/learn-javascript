578fc3fed5303741444a9e7f488141e87cf21b00
prachin007 <prachinnayak07@gmail.com>
2023-12-02 17:21:42 +0530

    almost google interface created

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -2,31 +2,68 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
+import {gsap} from "gsap";
 
+function Googlebar(){
+    let searchtext=()=>{
+        
+    }
+    return(
+        <div >
+            <input className='inpbar' placeholder='search' onClick={searchtext}/>
+        </div>
+    );
+}
+
+function Logos(){
+    return(
+        <div >
+            <h1>Google</h1>
+            <Googlebar/>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <div style={{display:"inline-block",margin:"20px"}} className='divbg'>
+                    <a href='https://www.tesla.com/' target='_blank' className='imgLinkCircle' 
+                    style={{display:"inline-block"}}>
+                        <img src="https://cdn-icons-png.flaticon.com/128/1724/1724639.png"
+                            style={{height:"40px"}}
+                        />
+                    </a>
+                    <p style={{marginLeft:"center"}}>tesla</p>
+                </div>
+                <div style={{display:"inline-block",margin:"20px"}}  className='divbg'>
+                    <a href='https://github.com/' target='_blank' className='imgLinkCircle' 
+                    style={{display:"inline-block"}}>
+                        <img src="https://cdn-icons-png.flaticon.com/128/6424/6424084.png"
+                            style={{height:"40px"}}
+                        />
+                    </a>
+                    <p style={{marginLeft:"center"}}>github</p>
+                </div>
+                <div style={{display:"inline-block",margin:"20px"}}  className='divbg' >
+                    <a href='https://twitter.com/' target='_blank' className='imgLinkCircle' 
+                    style={{display:"inline-block"}}>
+                        <img src="https://cdn-icons-png.flaticon.com/128/733/733579.png"
+                            style={{height:"40px"}}
+                        />
+                    </a>
+                    <p style={{margin:"center"}}>twitter</p>
+                </div>
+                <div style={{display:"inline-block",margin:"20px"}} className='divbg'>
+                    <a href='https://www.uber.com/in/en/' target='_blank' className='imgLinkCircle' 
+                    style={{display:"inline-block"}}>
+                        <img src="https://cdn-icons-png.flaticon.com/128/732/732135.png"
+                            style={{height:"40px"}}
+                        />
+                    </a>
+                    <p style={{margin:"center"}}>uber</p>
+                </div> 
+            </div>
+        </div>
+    );
+}
 
 export default function App(){
-  // let back="https://cdn-icons-png.flaticon.com/128/3916/3916837.png"
-  let [color,setColor] = useState('bisque')
-  let changeColor=(value)=>{
-    setColor(value)
-  }
-
-  return(
-    <div className="screen" style={{backgroundColor:color}}>
-      <div className='bstrip'>
-        <button className='butts'  
-          style={{backgroundColor:"bisque"}} 
-          onClick={()=>changeColor("bisque")}>
-          default
-          <img src="https://cdn-icons-png.flaticon.com/128/3916/3916837.png"
-            style={{height:"20px"}}
-          ></img>
-        </button>
-        
-        <button className='butts'  style={{backgroundColor:"white"}} onClick={()=>changeColor("white")}>white</button>
-        <button className='butts'  style={{backgroundColor:"black",color:"white"}} onClick={()=>changeColor("black")}>black</button>
-        <button className='butts'  style={{backgroundColor:"green"}} onClick={()=>changeColor("green")}>green</button>
-      </div>
-    </div>
-  );
+    return(
+        <Logos/>
+    );
 }
